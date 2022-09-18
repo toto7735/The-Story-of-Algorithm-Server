ebebc558b8c25f7485c93f8bf90cee80cad1b28b
BuildTools <unconfigured@null.spigotmc.org>
2022-09-18 17:47:42 +0900

    Commit

---
+++ start.bat
@@ -0,0 +1,3 @@
+:start
+java -Xms1G -Xmx1G -XX:+ParallelRefProcEnabled -XX:+UseG1GC -XX:MaxGCPauseMillis=50 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar paper-1.19-40.jar nogui
+goto start
